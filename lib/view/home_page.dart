f08f9cb55b4cbe77b183ac3c8c01b8fd49fffce9
t.kinuta <takashi.kinuta@bluetec.co.jp>
2024-01-22 12:32:23 +0900

    update pubspec

---
+++ lib/view/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+import 'package:countup_mvvm/view_model/home_page_notifier.dart';
+
+class HomePage extends ConsumerWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // state（状態）
+    final _homePageState = ref.watch(homePageProvider);
+    // provider（状態の操作）
+    final _homePageNotifier = ref.watch(homePageProvider.notifier);
+    return Scaffold(
+      appBar: AppBar(title: const Text('COUNTUP MVVM')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _homePageNotifier.resetAllCount,
+        child: const Icon(Icons.exposure_zero),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('Main Count ${_homePageState.mainCount}'),
+            onTap: _homePageNotifier.increaseMainCount,
+          ),
+          ListTile(
+            title: Text('Sub Count ${_homePageState.subCount}'),
+            onTap: _homePageNotifier.increaseSubCount,
+          ),
+        ],
+      ),
+    );
+  }
+}
